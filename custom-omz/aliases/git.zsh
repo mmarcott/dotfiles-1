171c9e45576fbfd7385e75c6fe2940997fbecda7
audibleblink <virtualsnyper@gmail.com>
2015-07-18 15:43:44 -0400

    set git alias to open all MC in atom

--- custom-omz/aliases/git.zsh
+++ custom-omz/aliases/git.zsh
@@ -1,7 +1,7 @@
 alias gt='git tag'
 
 # Open all merge conflicts
-alias fml='git diff --name-only | uniq | xargs subl'
+alias fml='git diff --name-only | uniq | xargs e'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 
