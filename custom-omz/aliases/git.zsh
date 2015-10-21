123c4e946877152e1cc516c9501c98723bc7a45b
Alex Flores <eflores@mdsol.com>
2015-10-21 12:16:04 -0400

    edits aliases

--- custom-omz/aliases/git.zsh
+++ custom-omz/aliases/git.zsh
@@ -1,7 +1,5 @@
 alias gt='git tag'
 
 # Open all merge conflicts
-alias fml='git diff --name-only | uniq | xargs e'
+alias fml='git diff --name-only | uniq | xargs atom'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-
-
--- custom-omz/aliases/system.zsh
+++ custom-omz/aliases/system.zsh
@@ -5,3 +5,5 @@ alias fixaudio='ps ax | grep coreaudiod | grep sbin | awk "{print $1}"'
 alias e=atom
 alias asi='tree -if | grep -v node_modules | grep -e .js$ | xargs fixmyjs --legacy'
 
+# enables command auto-correction
+eval "$(thefuck --alias)"
