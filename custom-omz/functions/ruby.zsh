d7904b368a5d336aa902ceea51592b1cf208ff43
Alex Flores <eflores@mdsol.com>
2015-10-27 15:41:06 -0400

    add sh function to append gem to gemfile

--- custom-omz/functions/ruby.zsh
+++ custom-omz/functions/ruby.zsh
@@ -1,2 +1,8 @@
 # Initiate rbenv
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+  
+# Adds gem to Gemfile. Must be in APP_ROOT  
+gemadd () {
+  gem=$1
+  echo "gem '$gem'" >> Gemfile
+}
