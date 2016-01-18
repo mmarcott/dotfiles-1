a1aa8492e59d42f3f0623dfd0f2712b9ad49c9ae
Alex Flores <eflores@mdsol.com>
2016-01-18 15:26:59 -0500

    use zelda hearts

--- battery.sh
+++ battery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-HEART='♥'
+HEART='❤︎ '
 
 if [[ `uname` == 'Linux' ]]; then
   current_charge=$(cat /proc/acpi/battery/BAT1/state | grep 'remaining capacity' | awk '{print $3}')
@@ -11,15 +11,15 @@ else
   total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
 fi
 
-charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
-if [[ $charged_slots -gt 10 ]]; then
-  charged_slots=10
+charged_slots=$(echo "(($current_charge/$total_charge)*5)+1" | bc -l | cut -d '.' -f 1)
+if [[ $charged_slots -gt 5 ]]; then
+  charged_slots=5
 fi
 
 printf '#[fg=colour9]'
 for i in `seq 1 $charged_slots`; do printf "$HEART"; done
 
-if [[ $charged_slots -lt 10 ]]; then
+if [[ $charged_slots -lt 5 ]]; then
   printf '#[fg=white]'
-  for i in `seq 1 $(echo "10-$charged_slots" | bc)`; do printf "$HEART"; done
+  for i in `seq 1 $(echo "5-$charged_slots" | bc)`; do printf "$HEART"; done
 fi
