34841a96d0cced26414609e59d144d3a634f2291
Alex Flores <eflores@mdsol.com>
2016-04-25 17:28:49 -0400

    abolish atom

--- custom-omz/aliases/git.zsh
+++ custom-omz/aliases/git.zsh
@@ -1,5 +1,5 @@
 alias gt='git tag'
 
 # Open all merge conflicts
-alias fml='git diff --name-only | uniq | xargs atom'
+alias fml='e -O $( git diff --name-only | uniq )'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
