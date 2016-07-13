bb48f243d05340b3f8e21c346df2fb3ee1b26e66
Alex Flores <eflores@mdsol.com>
2016-07-13 17:56:29 -0400

    delete docker, python, mdsol dotfiles

--- custom-omz/aliases/mdsol.zsh
+++
@@ -1,3 +0,0 @@
-alias jump='ssh -o ProxyCommand="ssh -W %h:%p 52.2.169.72"'
-alias dalton='cd ~/Code/Medidata/dalton'
-alias pm='cd ~/Code/Medidata/the_policy_machine'
--- custom-omz/functions/docker.zsh
+++
@@ -1,9 +0,0 @@
-docker-machine env default &> /dev/null
-if [[ $? -eq 0 ]]; then
-  eval "$(docker-machine env default)"
-fi
-
-docker_start () {
-  docker-machine start default && eval "$(docker-machine env default)"
-}
-
