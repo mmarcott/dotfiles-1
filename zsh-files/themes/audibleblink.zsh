8cc5a70b1c55b0dc89b94902084e3974bf78e9e0
Alex Flores <eflores@mdsol.com>
2016-07-16 17:06:48 -0400

    change git dirty sign

--- zsh-files/themes/audibleblink.zsh
+++ zsh-files/themes/audibleblink.zsh
@@ -3,7 +3,7 @@ autoload -U colors && colors
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 
-ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}☹%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg[red]%}⚠︎%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=" %{$fg[green]%}✓%{$reset_color%}"
 
 # Colors vary depending on time lapsed.
