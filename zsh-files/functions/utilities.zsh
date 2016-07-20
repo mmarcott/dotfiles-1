a6d87c8d237689bff0e2f19e549c3291562ad431
blink <virtualsnyper@gmail.com>
2016-07-19 22:42:09 -0400

    add extract function

---
+++ zsh-files/functions/utilities.zsh
@@ -0,0 +1,61 @@
+function extract {
+  if [ -z "$1" ]; then
+    # display usage if no parameters given
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+  else
+    if [ -f $1 ] ; then
+      # NAME=${1%.*}
+      # mkdir $NAME && cd $NAME
+      case $1 in
+        *.tar.bz2)   tar xvjf ../$1    ;;
+        *.tar.gz)    tar xvzf ../$1    ;;
+        *.tar.xz)    tar xvJf ../$1    ;;
+        *.lzma)      unlzma ../$1      ;;
+        *.bz2)       bunzip2 ../$1     ;;
+        *.rar)       unrar x -ad ../$1 ;;
+        *.gz)        gunzip ../$1      ;;
+        *.tar)       tar xvf ../$1     ;;
+        *.tbz2)      tar xvjf ../$1    ;;
+        *.tgz)       tar xvzf ../$1    ;;
+        *.zip)       unzip ../$1       ;;
+        *.Z)         uncompress ../$1  ;;
+        *.7z)        7z x ../$1        ;;
+        *.xz)        unxz ../$1        ;;
+        *.exe)       cabextract ../$1  ;;
+        *)           echo "extract: '$1' - unknown archive method" ;;
+      esac
+    else
+      echo "$1 - file does not exist"
+    fi
+  fi
+}
+
+
+# is x grep argument available?
+grep-flag-available() {
+  echo | grep $1 "" >/dev/null 2>&1
+}
+
+GREP_OPTIONS=""
+
+# color grep results
+if grep-flag-available --color=auto; then
+  GREP_OPTIONS+=" --color=auto"
+fi
+
+# ignore VCS folders (if the necessary grep flags are available)
+VCS_FOLDERS="{.bzr,CVS,.git,.hg,.svn}"
+
+if grep-flag-available --exclude-dir=.cvs; then
+  GREP_OPTIONS+=" --exclude-dir=$VCS_FOLDERS"
+elif grep-flag-available --exclude=.cvs; then
+  GREP_OPTIONS+=" --exclude=$VCS_FOLDERS"
+fi
+
+# export grep settings
+alias grep="grep $GREP_OPTIONS"
+
+# clean up
+unset GREP_OPTIONS
+unset VCS_FOLDERS
+unfunction grep-flag-available
