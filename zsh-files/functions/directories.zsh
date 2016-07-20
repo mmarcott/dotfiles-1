dfe4c34415bf7dfa67100aea1bc06bfd02d5470c
Alex Flores <eflores@mdsol.com>
2016-07-20 16:24:05 -0400

    update directory aliases

--- zsh-files/functions/directories.zsh
+++ zsh-files/functions/directories.zsh
@@ -8,68 +8,29 @@ alias -g ..='..'
 alias -g ...='../..'
 alias -g ....='../../..'
 alias -g .....='../../../..'
-alias -g ......='../../../../..'
 
 alias -- -='cd -'
 alias 1='cd -'
 alias 2='cd -2'
 alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
 
 alias md='mkdir -p'
-alias rd=rmdir
 alias d='dirs -v | head -10'
 
 # List directory contents
 alias lsa='ls -lah'
-alias l='ls -lah'
 alias ll='ls -lh'
 alias la='ls -lAh'
-
-# Push and pop directories on directory stack
-alias pu='pushd'
-alias po='popd'
+alias l='ls -lth'
+alias ls='ls -FGh'
 
 alias desktop='cd ~/Desktop'
-alias ls='ls -FGh'
-alias l='ls -lth'
+alias dot='cd ~/.dotfiles'
 
 which tree >/dev/null && alias tree='LS_COLORS="di=34:ln=35:so=32;40:ex=31" tree'
 
 # mkdir, cd into it
 mkcd () {
-  mkdir -p "$*"
-  cd "$*"
+  mkdir -p "$1"
+  cd "$1"
 }
-
-function open_command() {
-  emulate -L zsh
-  setopt shwordsplit
-
-  local open_cmd
-
-  # define the open command
-  case "$OSTYPE" in
-    darwin*)  open_cmd='open' ;;
-    cygwin*)  open_cmd='cygstart' ;;
-    linux*)   open_cmd='xdg-open' ;;
-    msys*)    open_cmd='start ""' ;;
-    *)        echo "Platform $OSTYPE not supported"
-              return 1
-              ;;
-  esac
-
-  # don't use nohup on OSX
-  if [[ "$OSTYPE" == darwin* ]]; then
-    $open_cmd "$@" &>/dev/null
-  else
-    nohup $open_cmd "$@" &>/dev/null
-  fi
-}
-
-alias open='open_command'
