08eaae82e75b12177983a4beb9c0358911eeaa83
Alex Flores <domain@me.com>
2016-07-27 19:08:12 -0400

    fix i3 notification scripts

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -1,10 +1,10 @@
-pavu_status=$(pulseaudio-ctl full-status)
-pavu_status=($pavu_status)
+#!/bin/bash
+pavu_status=($(pulseaudio-ctl full-status))
 
 volume=${pavu_status[0]}
 muted=${pavu_status[1]}
 
-if [ "$muted" == "yes" ]; then
+if [ "$muted" = "yes" ]; then
   echo '<span foreground="#Cd3f45">Mute</span>'
 else
   echo $volume
